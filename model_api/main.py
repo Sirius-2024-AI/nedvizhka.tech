d7575b2a945c711a07d0b2d41e1a66cdf52f299d
Qwest1204 <workemailfordaniil@gmail.com>
2024-07-20 15:09:42 +0300

    feat: add all other repository

---
+++ model_api/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Request, Response
+
+from scr.utils import *
+
+app = FastAPI()
+
+
+@app.get("/get_price")
+async def get_price(request:Request, response:Response, city, area, kitchen_area, total_rooms, floor, total_floors, build_date, material_type,
+                    remont, total_balcony, metro_name, metro_dist_km,distance_to_center):
+    dict_data = {'city': city,
+                 'area': area,
+                 'kitchen_area': kitchen_area,
+                 'total_rooms': total_rooms,
+                 'floor': floor,
+                 'total_floors': total_floors,
+                 'build_date': build_date,
+                 'material_type': material_type,
+                 'remont': remont,
+                 'total_balcony': total_balcony,
+                 'metro_name': metro_name,
+                 'metro_dist_km': metro_dist_km,
+                 'distance_to_center': distance_to_center}
+    return {"price": price_predict(dict_data)}

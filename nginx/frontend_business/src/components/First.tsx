9374a04f86efeaca73262224ad1239e5d45ae5ce
Qwest1204 <workemailfordaniil@gmail.com>
2024-07-21 18:48:08 +0300

    fix: move directories

---
+++ nginx/frontend_business/src/components/First.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+interface HeroProps {
+  heroImage: string;
+  buttonImage: string;
+}
+
+const Hero: React.FC<HeroProps> = ({ heroImage, buttonImage }) => {
+  return (
+    <section className="justify-center mt-14 w-full max-w-[1026px] max-md:mt-10 max-md:max-w-full max-md:px-5">
+      <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+        <div className="flex flex-col w-[67%] max-md:ml-0 max-md:w-full">
+          <div className="flex flex-col grow justify-center text-5xl font-bold text-white max-md:mt-10 max-md:max-w-full max-md:text-4xl">
+            <div className="flex flex-col justify-center py-1 pr-5 max-md:max-w-full max-md:text-4xl">
+              <h1 className="max-md:max-w-full max-md:text-4.2xl leading-none">
+                <span className="font-mabry leading-none">
+                  готовое решение <br></br>для автоматизации
+                </span>
+              </h1>
+              <h1 className="max-md:max-w-full max-md:text-4.2xl leading-none">
+                <span className="leading-none font-mabry bg-gradient-to-r from-[#0DB6B6] to-[#5454F1] inline-block text-transparent bg-clip-text">оценки недвижимости</span>
+              </h1>
+              <a className="font-normal mt-11 text-lg max-md:mt-10 max-md:max-w-full">
+                  Сервис на базе AI собственной разработки поможет генерировать отчеты моментально и без дополнительных издержек на сотрудников оценки недвижимости
+              </a>
+            </div>
+            <a href="#connect">
+              <img
+                loading="lazy"
+                src={buttonImage}
+                className="mt-36 max-w-full aspect-[4.76] w-[306px] max-md:mt-10"
+                alt="Начать оценку"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-col lg-ml-5 w-[33%] max-md:w-full">
+          <img
+            loading="lazy"
+            src={heroImage}
+            className="grow w-full aspect-[0.68] max-md:mt-10"
+            alt="Недвижимость"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ nginx/frontend_business/src/components/Header.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+interface HeaderProps {
+  logo: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ logo }) => {
+  return (
+    <header className="hidden md:flex items-center justify-between px-12 py-6 w-full text-lg text-white bg-white bg-opacity-10 max-w-[1026px] rounded-2xl md:rounded-full max-md:px-5 max-md:max-w-full">
+      <div className="flex w-1/4 max-md:w-full">
+        <img
+          loading="lazy"
+          src={logo}
+          className="shrink-0 max-w-full aspect-[7.14] w-[207px]"
+          alt="Logo"
+        />
+      </div>
+      <nav className="flex w-3/4 justify-around max-md:flex-col max-md:gap-3">
+        <a
+          href="#connect"
+          className="font-inter justify-center px-px py-1"
+        >
+          Подключить API
+        </a>
+        <a
+          href="/docs"
+          className="justify-center px-px py-1"
+        >
+          Документация
+        </a>
+        <a
+          href="/faq"
+          className="justify-center px-px py-1"
+        >
+          FAQ
+        </a>
+        <a
+          href="http://nedvizhka.tech/"
+          className="justify-center px-px py-1"
+        >
+          Для физических лиц
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
